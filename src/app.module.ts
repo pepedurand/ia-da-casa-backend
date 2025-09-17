6efe4de4229f1f6b4d69ddabda2c2d05eca3c4f7
Pedro Durand <webpedrod@gmail.com>
2025-09-16 22:40:38 -0300

    refactor: remove commented imports and unused code in app and chat modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-import { DatabaseModule } from './database/database.module';
-import { UsuarioModule } from './models/usuario/usuario.module';
-import { AuthModule } from './auth/auth.module';
+// import { DatabaseModule } from './database/database.module';
+// import { UsuarioModule } from './models/usuario/usuario.module';
+// import { AuthModule } from './auth/auth.module';
 import { ChatModule } from './models/chat/chat.module';
 
 @Module({
-  imports: [DatabaseModule, UsuarioModule, AuthModule, ChatModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    // DatabaseModule,
+    // UsuarioModule,
+    // AuthModule,
+    ChatModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/models/chat/chat.module.ts
+++ src/models/chat/chat.module.ts
@@ -1,18 +1,23 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
+// import { TypeOrmModule } from '@nestjs/typeorm';
 import { ChatController } from './chat.controller';
 import { ChatService } from './chat.service';
-import { ContentStore } from './content.store';
-import { InformacoesEntity } from './entities/Informacoes.entity';
+// import { ContentStore } from './content.store';
+// import { InformacoesEntity } from './entities/Informacoes.entity';
 import { ScheduleTool } from './tools/schedule.tool';
-import { InformacoesRepository } from './repositories/Informacoes.repository';
+// import { InformacoesRepository } from './repositories/Informacoes.repository';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([InformacoesEntity]), // conecta Entity ao TypeORM
-  ],
+  // imports: [
+  //   TypeOrmModule.forFeature([InformacoesEntity]), // conecta Entity ao TypeORM
+  // ],
   controllers: [ChatController],
-  providers: [ChatService, ContentStore, ScheduleTool, InformacoesRepository],
+  providers: [
+    ChatService,
+    // ContentStore,
+    ScheduleTool,
+    //  InformacoesRepository
+  ],
   exports: [ChatService],
 })
 export class ChatModule {}
