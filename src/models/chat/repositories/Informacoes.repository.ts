67f8461105f9b0e132760713f9ff82ed99f815f4
Pedro Durand <webpedrod@gmail.com>
2025-09-17 11:02:10 -0300

    refactor: remove código comentado nas classes ContentStore e InformacoesRepository

--- src/models/chat/repositories/Informacoes.repository.ts
+++ src/models/chat/repositories/Informacoes.repository.ts
@@ -1,29 +1,29 @@
-import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { InformacoesEntity } from '../entities/Informacoes.entity';
+// import { Injectable } from '@nestjs/common';
+// import { InjectRepository } from '@nestjs/typeorm';
+// import { Repository } from 'typeorm';
+// import { InformacoesEntity } from '../entities/Informacoes.entity';
 
-@Injectable()
-export class InformacoesRepository {
-  constructor(
-    @InjectRepository(InformacoesEntity)
-    private readonly repo: Repository<InformacoesEntity>,
-  ) {}
+// @Injectable()
+// export class InformacoesRepository {
+//   constructor(
+//     @InjectRepository(InformacoesEntity)
+//     private readonly repo: Repository<InformacoesEntity>,
+//   ) {}
 
-  async getByKind(kind: string): Promise<any> {
-    const row = await this.repo.findOne({
-      where: { kind },
-      order: { version: 'DESC' },
-    });
-    return row?.data;
-  }
+//   async getByKind(kind: string): Promise<any> {
+//     const row = await this.repo.findOne({
+//       where: { kind },
+//       order: { version: 'DESC' },
+//     });
+//     return row?.data;
+//   }
 
-  async save(kind: string, data: any): Promise<InformacoesEntity> {
-    const last = await this.repo.findOne({
-      where: { kind },
-      order: { version: 'DESC' },
-    });
-    const version = last ? last.version + 1 : 1;
-    return this.repo.save({ kind, data, version });
-  }
-}
+//   async save(kind: string, data: any): Promise<InformacoesEntity> {
+//     const last = await this.repo.findOne({
+//       where: { kind },
+//       order: { version: 'DESC' },
+//     });
+//     const version = last ? last.version + 1 : 1;
+//     return this.repo.save({ kind, data, version });
+//   }
+// }
